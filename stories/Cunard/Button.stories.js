b8532944311e22e9b2d20c96208f28b60cb74119
Mirko Oricci <mirko.oricci@eclipsegroup.co.uk>
2022-09-29 14:59:00 +0100

    add figma to storybook

--- stories/Cunard/Button.stories.js
+++ stories/Cunard/Button.stories.js
@@ -11,9 +11,16 @@ CTA.args = {
   mode: "primary",
   label: "Cunard CTA",
 };
+CTA.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/XX778VaSZ3F1pAVlCPxzM4/testing-link-to-storybook?node-id=0%3A1',
+  },
+};
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   mode: "secondary",
   label: "Cunard Button Secondary",
 };
+
