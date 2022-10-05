7e20b7532c896a90d365244be4478e2843b9637a
Mirko Oricci <mirko.oricci@eclipsegroup.co.uk>
2022-10-05 13:21:57 +0100

    initiate carousel with scroll functionality

--- stories/Cunard/Carousel.js
+++ stories/Cunard/Carousel.js
@@ -1,10 +1,14 @@
 import "./Carousel.css";
 
-
 export const createCarouselContainer = (args) => {
-  console.log("args :>> ", args);
   const container = document.createElement("li");
+  container.style.height = args.height;
+  container.style.width = args.width;
   container.className = "eclipse-carousel__container";
+  args.picturesUrls.map((pictureUrl) => {
+    args = {...args, pictureUrl}
+    container.appendChild(createPicture(args));
+  });
   return container;
 };
 
@@ -13,6 +17,21 @@ export const createCarousel = (args) => {
   carousel.className = ["eclipse-carousel"];
   carousel.style.height = args.height;
   carousel.style.width = args.width;
-  carousel.appendChild(createCarouselContainer());
+  carousel.appendChild(createCarouselContainer(args));
   return carousel;
+};
+
+export const createPicture = (args) => {
+  const picture = document.createElement("picture");
+  picture.style.height = args.height;
+  picture.style.width = args.width;
+  const source = document.createElement("source");
+  source.srcset = args.pictureUrl;
+  const img = document.createElement('img')
+  img.style.height = args.height;
+  img.style.width = args.width;
+  img.loading = 'lazy'
+  picture.appendChild(source);
+  picture.appendChild(img);
+  return picture;
 };
--- stories/Cunard/Carousel.stories.js
+++ stories/Cunard/Carousel.stories.js
@@ -4,11 +4,15 @@ export default {
   title: "Cunard/Carousel",
 };
 
-
 const Template = (args) => createCarousel(args);
 
 export const Default = Template.bind({});
 Default.args = {
-  width: '500px',
-  height: '300px'
-}
+  width: "500px",
+  height: "300px",
+  picturesUrls: [
+    "https://assets.traveltriangle.com/blog/wp-content/uploads/2018/03/acj-2003-beautiful-landscapes-around-the-world.jpg",
+    'http://cdn.cnn.com/cnnnext/dam/assets/190517091026-07-unusual-landscapes-travel.jpg',
+    'https://media.macphun.com/img/uploads/customer/blog/504/15360610675b8e6e8b52bd36.49629027.jpg?q=85&w=1680'
+  ],
+};
